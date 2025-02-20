c0a1777a5ed0015e8439623415f780bcf8f693cd
sabina tamang <syangtansabia8@gmail.com>
2025-02-20 10:44:01 +0545

    add new files

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+EXTERNAL_APPS=[
+    'main',
+]
+INSTALLED_APPS.extend(EXTERNAL_APPS)
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -116,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL='/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
